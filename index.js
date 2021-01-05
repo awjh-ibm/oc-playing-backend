0eec6a1df36144835726bcf7f826997baf9b57f1
Andrew Hurt <andrew.hurt1@ibm.com>
2021-01-05 15:43:23 +0000

    update start
    Signed-off-by: Andrew Hurt <andrew.hurt1@ibm.com>

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.BACKEND_PORT || 8080;
 var ip = process.env.BACKEND_IP || '127.0.0.1'; 
 
 app.get('/', (req, res) => {
-    res.json({hello: 'world'});
+    res.json({timestamp: new Date().getTime()});
 });
 
 app.listen(port, ip, () => {
