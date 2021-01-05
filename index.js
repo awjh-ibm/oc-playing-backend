4aee5b4a7047dac7d24b876291e5664a328664f9
Andrew Hurt <andrew.hurt1@ibm.com>
2021-01-05 16:21:59 +0000

    fix env vars for server list
    Signed-off-by: Andrew Hurt <andrew.hurt1@ibm.com>

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const app = express();
 
-var port = process.env.BACKEND_PORT || 8080;
-var ip = process.env.BACKEND_IP || '127.0.0.1'; 
+var port = process.env.OPENSHIFT_NODEJS_PORT || 8080;
+var ip = process.env.OPENSHIFT_NODEJS_IP || '127.0.0.1'; 
 
 app.get('/', (req, res) => {
     res.json({timestamp: new Date().getTime()});
